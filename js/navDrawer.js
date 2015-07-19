8bb25eef9a601c8b3ec4091bd23b8ed39502ee77
kakawup <w.kmkymt@gmail.com>
2015-07-19 23:34:03 +0900

    Built %souceName% from commit ec4f83a on branch %source Branch%

--- js/navDrawer.js
+++ js/navDrawer.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-  var $navBody, $navClose, $navShadow, duration;
-  $navClose = $("#drawer-icon, #drawer-shade, #drawer-wrap nav a");
-  $navBody = $("#drawer-wrap nav");
-  $navShadow = $("#drawer-shade");
-  duration = 200;
-  $navClose.click(function() {
-    $navBody.animate({
-      width: "toggle"
-    }, duration);
-    $navShadow.fadeToggle(duration);
-  });
+  var $navBody, $navClose, $navShadow, duration, mobileWidth, windowWidth;
+  windowWidth = $(window).width();
+  mobileWidth = 680;
+  if (windowWidth <= mobileWidth) {
+    $navClose = $("#drawer-icon, #drawer-shade, #drawer-wrap nav a");
+    $navBody = $("#drawer-wrap nav");
+    $navShadow = $("#drawer-shade");
+    duration = 200;
+    $navClose.click(function() {
+      $navBody.animate({
+        width: "toggle"
+      }, duration);
+      $navShadow.fadeToggle(duration);
+    });
+  }
 });
